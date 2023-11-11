47739e7d2937c183d7041676f9b71e5273a6381b
corposhare <workforcorposhare@gmail.com>
2023-11-11 14:28:40 +0530

    new

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const productRouter = require('./routes/product');
+require('dotenv').config()
 
 // const index = fs.readFileSync('index.html', 'utf-8');
 
@@ -11,6 +12,7 @@ const server = express();
 server.use(express.json())
 server.use('/api/v1',productRouter.router)
 
-server.listen(8080,()=>{
+// console.log('env',process.env.DB_PASSWORD)
+server.listen(process.env.PORT,()=>{
     console.log("Server Started")
 })
